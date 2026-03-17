1a1e230d0a6bd0eb631b41bb0d472698c4da222d
Amanda Tomaz <tomazamanda26@gmail.com>
2026-03-17 16:56:47 -0300

    Add files via upload

---
+++ Algoritmos de Programação/Aula1103/duasVariaveis.py
@@ -0,0 +1,9 @@
+a = int(input("Digite um número inteiro: "))
+b = int(input("Digite outro número inteiro: "))
+
+if a == b:
+    c = a + b
+    print(c)
+else:
+    c = a * b
+    print(c)
---
+++ Algoritmos de Programação/Aula1103/multiplo.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um número: "))
+
+if num % 5 == 0:
+    print(f"O número {num} é múltiplo de 5")
+else:
+    print(f"O número {num} não é múltiplo de 5")
